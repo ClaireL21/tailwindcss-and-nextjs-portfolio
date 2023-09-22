d50c08a68c9a5c54d011f26b5d4b3a6cb95d1f01
ClaireL21 <53claire@gmail.com>
2023-09-22 02:08:37 -0400

    added demo for micromaya

--- components/projectsections/MicroMaya.tsx
+++ components/projectsections/MicroMaya.tsx
@@ -26,94 +26,7 @@ const MicroMayaSection = () => {
                 data structure used to represent 3D meshes, performing subdivisions to 
                 an arbitrary mesh structure, and applying a skeleton to a mesh 
                 to allow transformations to the mesh.
-                
             </div>
-
-            <br/>
-            <h1 className="text-left font-bold text-2xl">
-                Half Edge Mesh
-            </h1>
-
-            <div className="mt-2"> The first step in creating Micro Maya was determining how to represent 3D meshes so 
-                that they could be transformed and distorted easily. 
-
-                Each mesh structure has three main parts - vertices, edges, and faces. 
-
-                Because my goal was to create a mini version of Maya, I wanted to make 
-                basic mesh editing functions availabe, such as moving vertices/edges/faces and 
-                changing their colors.
-            </div>
-
-            <div className="min-h-screen flex items-center justify-center"> 
-                <div className="grid grid-cols-2 grid-rows-1 gap-8">
-                    <Image 
-                        className="rounded-xl"
-                        src="/MicroMayaDemos/Triangulate 1.png"
-                        alt="" 
-                        width={150} 
-                        height={150}>
-                    </Image>
-
-                    <Image 
-                        className="rounded-xl"
-                        src="/MicroMayaDemos/Catmull Thumb.png"
-                        alt="" 
-                        width={200} 
-                        height={200}>
-                    </Image>
-                </div>
-            </div>
-
-            <div className="mt-8">
-                <video autoPlay controls style={{ width: '1000px', height: '500px' }}>
-                    <source src="/MicroMayaDemos/Micro Maya Half edge cube.mp4" />
-                </video>
-            </div>
-
-            <div className="mt-8">
-                <video autoPlay controls style={{ width: '1000px', height: '500px' }}>
-                    <source src="/MicroMayaDemos/Micro Maya Catmull Clark subdivision.mp4" />
-                </video>
-            </div>
-
-            
-
-            <h1 className="text-left font-bold text-2xl">
-                Catmull Clark Subdivision
-            </h1>
-
-            <div className="min-h-screen flex items-center justify-center"> 
-                <div className="grid grid-cols-3 grid-rows-1 gap-8">
-                    <Image 
-                        className="rounded-xl"
-                        src="/MicroMayaDemos/Subdivision 3.png"
-                        alt="" 
-                        width={100} 
-                        height={100}>
-                    </Image>
-
-                    <Image 
-                        className="rounded-xl"
-                        src="/MicroMayaDemos/Subdivision 2.png"
-                        alt="" 
-                        width={100} 
-                        height={100}>
-                    </Image>
-
-                    <Image 
-                        className="rounded-xl"
-                        src="/MicroMayaDemos/Subdivision 1.png"
-                        alt="" 
-                        width={100} 
-                        height={100}>
-                    </Image>
-                </div>
-
-            </div>
-
-            <h1 className="text-left font-bold text-2xl">
-                Skeletons & Skinning
-            </h1>
 
             <div className="flex flex-row align-bottom space-x-4">
                 {tags.map((item, idx) => {
@@ -126,6 +39,150 @@ const MicroMayaSection = () => {
                     )
                 })}
             </div>
+
+            <br/>
+            <h1 className="text-left font-bold text-2xl">
+                Half Edge Mesh
+            </h1>
+            <div className="mt-2"> The first step in creating Micro Maya was determining how to represent 3D meshes so 
+                that they could be transformed and distorted easily. 
+
+                Each mesh structure has three main parts - vertices, edges, and faces. 
+
+                Because my goal was to create a mini version of Maya, I wanted to make 
+                basic mesh editing functions availabe, such as moving vertices/edges/faces and 
+                changing their colors.
+
+            </div>
+
+             <br/>
+
+            <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+                <div className="md:w-3/5 text:left">
+                    <div>
+                        To do this, I chose to represent meshes using a half edge data structure.
+                        Half edges are directed edges that frame a face. Each half edge stores
+                        a pointer to the face directly left of it, the next half edge in the ring of 
+                        edges, its symmetric half edge, and the vertex between itself and the next 
+                        half edge. This information allows for more convenient traversal
+                        around the data structure, making operations like edge splitting
+                        and subdivision easier.
+
+                        I took advantage of this half edge data structure to implement triangulation.
+                    </div>
+
+                    <br/>
+
+                    <div>
+                        To the right is an example showing the process of triangulation on the face of a dodecahedron.
+                    </div>
+                </div>
+                <div className="md:w-2/5 text-right flex item-center justify-center mt-8">
+                    <video loop autoPlay muted className="h-[200px] w-auto">
+                        <source src="/MicroMayaDemos/Catmull triangulation demo.mp4" />
+                    </video>
+                </div>
+            </div>
+
+            <br/>
+
+            <div>
+                In the demo, I first select the front face of the dodecahedron. 
+                (The color of the face transitions from green to yellow to illustrate its shape before triangulation).
+                Then, after triangulation, the pentagon becomes split so that the face is now composed of triangles.
+                This is done by creating two new faces (for a total of three triangular faces) and four new half
+                edges. Because our half edge data structure allows for easy traversal, we can fix the pointers
+                of our half edges easily and successfully split the face into three.
+            </div>
+
+            <h1 className="mt-8 text-left font-bold text-2xl">
+                Catmull-Clark Subdivision
+            </h1>
+
+            <div className="mt-2">
+                Another nice operation of Micro Maya is Catmull-Clark Subdivision, a method 
+                of smoothing a mesh. Catmull-Clark works by taking the centroid of each face, 
+                computing the midpoint of each edge in the mesh, and then using these values 
+                to smooth the original vertices. Each face in the initial mesh gets split into
+                a number of quadrangle faces equal to the original number of vertices.
+            </div>
+
+            <br/>
+
+            <div>
+                Below is a demonstration of a series of three Catmull-Clark subdivisions,
+                each one bringing the initial cube closer to a sphere.
+            </div>
+
+            <div className="flex items-center justify-center"> 
+                <div className="mt-8 md:grid md:grid-cols-2 md:grid-rows-1 md:gap-8">
+                    <video loop autoPlay muted className="h-[200px] w-auto">
+                        <source src="/MicroMayaDemos/Micro Maya Subdivision Demo.mp4" />
+                    </video>
+
+                    <div className="mt-8 flex items-center justify-center"> 
+                        <div className="grid grid-cols-3 grid-rows-1 gap-8">
+                            <Image 
+                                className="rounded-xl"
+                                src="/MicroMayaDemos/Subdivision 3.png"
+                                alt="" 
+                                width={100} 
+                                height={100}>
+                            </Image>
+
+                            <Image 
+                                className="rounded-xl"
+                                src="/MicroMayaDemos/Subdivision 2.png"
+                                alt="" 
+                                width={100} 
+                                height={100}>
+                            </Image>
+
+                            <Image 
+                                className="rounded-xl"
+                                src="/MicroMayaDemos/Subdivision 1.png"
+                                alt="" 
+                                width={100} 
+                                height={100}>
+                            </Image>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h1 className="mt-10 text-left font-bold text-2xl">
+                Skeletons & Skinning
+            </h1>
+
+            <div className="mt-2">
+                My favorite part about this project was attaching a skeleton to a 3D mesh and using it 
+                transform the mesh. Each skeleton is composed of a hierarchy of joints. When loaded in,
+                a skeleton can be moved around without affecting the mesh. However, the skeleton can be 
+                attached to the object by clicking the 'Skinning' button, and then any transformations
+                applied to the joints will be applied to the mesh as well.
+            </div>
+            
+            <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+                <div className="md:w-3/5 text:left">
+                    <Image 
+                        className="rounded-xl"
+                        src="/MicroMayaDemos/gui screen.png"
+                        alt="" 
+                        width={500} 
+                        height={500}>
+                    </Image>
+                </div>
+
+                <div className="md:w-2/5 text:right">
+                    <div className="flex item-center justify-center">
+                        <video loop autoPlay muted className="h-[200px] w-auto">
+                            <source src="/MicroMayaDemos/Cow Demo Cropped.mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+            
+
         </section>
     )
 }
